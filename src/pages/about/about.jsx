31063aa3d5eec3e817f47297f87a5c8563a672d1
SANJAY <sanjaypk986@gmail.com>
2024-08-01 10:01:58 +0530

    fixed some errors

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./about.css";
 import avatar from "../../assets/avatar7.png";
+
 const About = () => {
   return (
     <main>
       <section className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 mx-auto p-4 gap-2 lg:gap-4 lg:w-3/4">
-          <div className="text-gray-300 flex flex-col justify-center  gap-4">
-            <h2 className="text-2xl lg:text-3xl font-semibold text-start ">
+          <div className="text-gray-300 flex flex-col justify-center gap-4">
+            <h2 className="text-2xl lg:text-3xl font-semibold text-start">
               <span className="text-custom-green font-semibold mr-2">
                 &#8212;
               </span>
@@ -25,20 +26,22 @@ const About = () => {
             </p>
             <div className="grid md:grid-cols-2 gap-4 justify-center md:justify-around sm:w-3/4">
               <div className="flex justify-center items-center animate-bounce">
-                <a href="https://drive.google.com/uc?export=download&id=1_gOxcRelZestEdt6zCa_6JxVqqsASOmo" className="primary-button card-shadow">Download CV</a>
+                <a href="https://drive.google.com/uc?export=download&id=1_gOxcRelZestEdt6zCa_6JxVqqsASOmo" className="primary-button card-shadow">
+                  Download CV
+                </a>
               </div>
               <div className="flex items-center gap-4">
-                <a href="https://www.linkedin.com/in/sanjay-pk-93884a213/" className="social-icon card-shadow ">
-                  <i class="fa-brands fa-linkedin"></i>
+                <a href="https://www.linkedin.com/in/sanjay-pk-93884a213/" className="social-icon card-shadow">
+                  <i className="fa-brands fa-linkedin"></i>
                 </a>
                 <a href="https://github.com/Sanjaypk986" className="social-icon card-shadow">
-                  <i class="fa-brands fa-github"></i>
+                  <i className="fa-brands fa-github"></i>
                 </a>
-                <a href='https://gitlab.com/sanjaypk986' className="social-icon card-shadow">
-                  <i class="fa-brands fa-gitlab"></i>
+                <a href="https://gitlab.com/sanjaypk986" className="social-icon card-shadow">
+                  <i className="fa-brands fa-gitlab"></i>
                 </a>
                 <a href="https://www.instagram.com/s__n_j_a__y?igsh=MXNmZHFlMzN1emZrcA==" className="social-icon card-shadow">
-                  <i class="fa-brands fa-instagram"></i>
+                  <i className="fa-brands fa-instagram"></i>
                 </a>
               </div>
             </div>
