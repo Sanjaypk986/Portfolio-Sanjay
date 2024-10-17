591fd926df252d65b2cf71de5182f4111bd46a47
SANJAY <sanjaypk986@gmail.com>
2024-10-17 21:29:44 +0530

    added a project

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./projects.css";
 import project1 from "../../assets/project1.svg";
+import { Link } from "react-router-dom";
 const Projects = () => {
   return (
     <main>
-      <section className="container mx-auto text-gray-300">
+      <section className="projects container mx-auto text-gray-300">
         <h2 className="text-xl sm:text-3xl font-semibold text-center relative">
         <span className="absolute inset-x-0 bottom-0 h-1 bg-gray-700 rounded-full mx-auto w-1/4 transform translate-y-2"></span>
           <span className="text-custom-green font-semibold mr-2">&#8212;</span>
@@ -12,7 +13,8 @@ const Projects = () => {
           <span className="text-custom-green font-bold">Define Me</span>
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 my-2 md:grid-cols-3 mx-auto p-4 gap-2 md-w-full lg:gap-4 lg:w-4/5">
-          <div className="my-2">
+          
+          <Link to={'https://film-flick-movie-app.vercel.app/'} className="my-2">
             <div className="card bg-base-100 skill-icon relative overflow-hidden">
               <div className="card-body">
                 <h2 className="card-title">FilmFlick</h2>
@@ -49,7 +51,7 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
 
           <div className="my-2">
             <div className="card bg-base-100 skill-icon relative overflow-hidden">
@@ -139,6 +141,53 @@ const Projects = () => {
               </div>
             </div>
           </div>
+          <Link to={'https://spicezy-food-order.vercel.app/'} className="my-2">
+            <div className="card bg-base-100 skill-icon relative overflow-hidden">
+              <div className="card-body">
+                <h2 className="card-title">Spicezy</h2>
+                <p className="card-description">
+                Spicezy is a Food delivery web app with Restaurant and Admin panel sections.
+                </p>
+              </div>
+              <figure>
+                <img src={project1} alt="projects" />
+              </figure>
+              <div className="slide-in-overlay">
+                <div className="slide-in-content">
+                  <h2 className="slide-in-title text-custom-green font-bold my-1">
+                    Project Details
+                  </h2>
+                  <p className="my-1 text-start">
+                    <strong className="text-custom-green font-bold">
+                      Description:
+                    </strong>
+                    <span className="text-custom-light-gray">
+                      {" "}
+                      Restaurants, food menus, orders, and payment options."
+                    </span>
+                  </p>
+                  <p className="my-1 text-start">
+                    <strong className="text-custom-green font-bold">
+                      Technology Used:
+                    </strong>
+                    <span className="text-custom-light-gray">
+                      {" "}
+                      MERN Stack, Tailwind, Bcrypt, Redux, JWT
+                    </span>
+                  </p>
+                  <p className="my-1 text-start">
+                    <strong className="text-custom-green font-bold">
+                      Features:
+                    </strong>
+                    <span className="text-custom-light-gray">
+                      {" "}
+                       Add items to the cart, place orders,
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Link>
         </div>
       </section>
     </main>
